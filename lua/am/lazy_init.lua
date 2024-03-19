ed0c88521ed83c73f20124d2e4bd0c82857ae6b8
Andre Mæland <andre.maeland@netpower.no>
2024-03-19 21:42:54 +0100

    up

--- lua/am/lazy_init.lua
+++ lua/am/lazy_init.lua
@@ -11,4 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("am.lazy")
+require("lazy").setup("am.plugins", { change_detection = { enabled = false } })

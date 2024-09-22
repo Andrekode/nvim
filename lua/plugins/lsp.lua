55d9c25f5e5b4bc05f0c5c1f184be72ce0167f98
Andre Mæland <andre.maeland@netpower.no>
2024-09-22 14:19:30 +0200

    u

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -40,7 +40,6 @@ return {
                 ensure_installed = {
                     "lua_ls",
                     "rust_analyzer",
-                    "tsserver",
                 },
                 handlers = {
                     function(server_name)

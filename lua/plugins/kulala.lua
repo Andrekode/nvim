aacff66f627af768e281212406879414e90839af
Andre Maeland <andre.maeland@netpower.no>
2026-09-01 15:12:44 +0700

    kulala: auto-select env from .kulala-env, raise response cap
    pick up the nearest .kulala-env walking upward from the buffer and
    switch to it on open. bump max_response_size to 10MB.

--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -4,10 +4,32 @@ return {
 	ft = { "http", "rest" },
 	opts = {
 		ui = {
-			max_response_size = 42768,
+			max_response_size = 10485760,
 		},
 		contenttypes = {
 			["sosi%+json"] = "application/json",
 		},
 	},
+	config = function(_, opts)
+		require("kulala").setup(opts)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "http", "rest" },
+			callback = function(args)
+				local kulala = require("kulala")
+				local dir = vim.fs.dirname(vim.api.nvim_buf_get_name(args.buf))
+				local found = vim.fs.find(".kulala-env", { path = dir, upward = true })[1]
+				if found then
+					local f = io.open(found, "r")
+					if f then
+						local env = (f:read("*a") or ""):gsub("%s+", "")
+						f:close()
+						if env ~= "" and env ~= kulala.get_selected_env() then
+							kulala.set_selected_env(env)
+						end
+					end
+				end
+				vim.notify("env: " .. (kulala.get_selected_env() or "none"), vim.log.levels.INFO, { title = "kulala" })
+			end,
+		})
+	end,
 }
